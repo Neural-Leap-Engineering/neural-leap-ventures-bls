1a2ce5a4e750a2d1fc49cf1a17d3ce13c4a84419
Neural-Leap-Engineering <60061519+Neural-Leap-Engineering@users.noreply.github.com>
2024-05-17 21:56:54 +0530

    Update check-db-table.js

--- server/check-db-table.js
+++ server/check-db-table.js
@@ -10,26 +10,35 @@ const pool = new Pool({
 });
 
 exports.handler = async (event, context) => {
-    try {
-        // SQL query to retrieve data
-        const selectQuery = 'SELECT job_id, job_title FROM test';
-        
-        // Executing the SQL command
-        const result = await pool.query(selectQuery);
-        
-        // Fetch all rows from the database
-        const records = result.rows;
-
-        // Sending the records as response
-        return {
-            statusCode: 200,
-            body: JSON.stringify(records),
-        };
-    } catch (error) {
-        console.error(error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Error retrieving data' }),
-        };
-    }
+    return {
+        statusCode: 200,
+        body: "Hello, world!"
+    };
 };
+
+
+
+// exports.handler = async (event, context) => {
+//     try {
+//         // SQL query to retrieve data
+//         const selectQuery = 'SELECT job_id, job_title FROM test';
+        
+//         // Executing the SQL command
+//         const result = await pool.query(selectQuery);
+        
+//         // Fetch all rows from the database
+//         const records = result.rows;
+
+//         // Sending the records as response
+//         return {
+//             statusCode: 200,
+//             body: JSON.stringify(records),
+//         };
+//     } catch (error) {
+//         console.error(error);
+//         return {
+//             statusCode: 500,
+//             body: JSON.stringify({ error: 'Error retrieving data' }),
+//         };
+//     }
+// };
