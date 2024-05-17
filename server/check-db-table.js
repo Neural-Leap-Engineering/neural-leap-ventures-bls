f8e0b68ae8e4ee12b83edff5ef83be9af466762f
Neural-Leap-Engineering <60061519+Neural-Leap-Engineering@users.noreply.github.com>
2024-05-17 22:32:50 +0530

    Update check-db-table.js

--- server/check-db-table.js
+++ server/check-db-table.js
@@ -1,34 +1,44 @@
+require('dotenv').config();
+const { Pool } = require('pg');
 
-exports.handler = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: "Hello, world!"
-    };
-};
-
-
+const pool = new Pool({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
 // exports.handler = async (event, context) => {
-//     try {
-//         // SQL query to retrieve data
-//         const selectQuery = 'SELECT job_id, job_title FROM test';
-        
-//         // Executing the SQL command
-//         const result = await pool.query(selectQuery);
-        
-//         // Fetch all rows from the database
-//         const records = result.rows;
-
-//         // Sending the records as response
-//         return {
-//             statusCode: 200,
-//             body: JSON.stringify(records),
-//         };
-//     } catch (error) {
-//         console.error(error);
-//         return {
-//             statusCode: 500,
-//             body: JSON.stringify({ error: 'Error retrieving data' }),
-//         };
-//     }
+//     return {
+//         statusCode: 200,
+//         body: "Hello, world!"
+//     };
 // };
+
+
+
+exports.handler = async (event, context) => {
+    try {
+        // SQL query to retrieve data
+        const selectQuery = 'SELECT job_id, job_title FROM test';
+        
+        // Executing the SQL command
+        const result = await pool.query(selectQuery);
+        
+        // Fetch all rows from the database
+        const records = result.rows;
+
+        // Sending the records as response
+        return {
+            statusCode: 200,
+            body: JSON.stringify(records),
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Error retrieving data' }),
+        };
+    }
+};
